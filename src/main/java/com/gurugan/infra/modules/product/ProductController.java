fd4804715a6291dd87a234327e3e652adfd320d1
kiloward96 <kiloward96@gmail.com>
2022-11-09 10:51:01 +0900

    commit

--- src/main/java/com/gurugan/infra/modules/product/ProductController.java
+++ src/main/java/com/gurugan/infra/modules/product/ProductController.java
@@ -1,15 +1,12 @@
 package com.gurugan.infra.modules.product;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
